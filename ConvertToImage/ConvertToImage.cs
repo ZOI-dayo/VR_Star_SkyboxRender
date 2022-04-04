98d27d38baf93883c66b3124ad9e8c50a669b4c4
zoi <zoi@zoizoi.net>
2022-04-04 09:41:55 +0900

    出力調整

--- ConvertToImage/ConvertToImage.cs
+++ ConvertToImage/ConvertToImage.cs
@@ -10,7 +10,7 @@ public class ConvertToImage
   private const int ImageHeight = 4096;
   private const string DataPath = @"F:\out\out.bin";
   private const string OutFile = @"F:\out\out.png";
-  private const double Offset = 0.2;
+  private const double Offset = 0.002;
 
   public static void Main()
   {
@@ -32,19 +32,41 @@ public class ConvertToImage
       var g = dataStream.ReadDouble();
       var b = dataStream.ReadDouble();
       var weight = Math.Max(Math.Max(r, g), b) / Offset;
+      
+      if (double.IsNaN(weight)) continue;
 
-      byte RoundToByte(double source) => Convert.ToByte((int)Math.Round(Math.Max(source, 1) * 255));
+      byte RoundToByte(double source)
+      {
+        try
+        {
+          return Convert.ToByte((int)Math.Round(Math.Min(source, 1) * 255));
+        }
+        catch(Exception e)
+        {
+          Console.WriteLine(r);
+          Console.WriteLine(g);
+          Console.WriteLine(b);
+          Console.WriteLine(source);
+          throw e;
+        }
+      }
 
-      imageData[count * 4 + 0] = RoundToByte(r / weight);
-      imageData[count * 4 + 1] = RoundToByte(g / weight);
-      imageData[count * 4 + 2] = RoundToByte(b / weight);
-      imageData[count * 4 + 3] = RoundToByte(weight);
+      imageData[count * 4 + 1] = RoundToByte(weight != 0 ? r / weight : 0);
+      imageData[count * 4 + 2] = RoundToByte(weight != 0 ? g / weight : 0);
+      imageData[count * 4 + 3] = RoundToByte(weight != 0 ? b / weight : 0);
+      imageData[count * 4 + 0] = RoundToByte(1 - Math.Min(weight, 1));
       count++;
     }
 
-    var ms = new MemoryStream(imageData);
-    var bm = new Bitmap(ms);
-    bm.Save(OutFile, ImageFormat.Png);
+    Bitmap bmp = new Bitmap(ImageHeight * 2, ImageHeight);
+
+    Rectangle rect = new Rectangle(0, 0, ImageHeight * 2, ImageHeight);
+    System.Drawing.Imaging.BitmapData
+      bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+    IntPtr ptr = bmpData.Scan0;
+    Marshal.Copy(imageData, 0,ptr,imageData.Length);
+    bmp.UnlockBits(bmpData);
+    bmp.Save(OutFile, ImageFormat.Png);
 
     logSWriter.WriteLine($"End: {DateTime.Now.ToString(new CultureInfo("ja-JP"))}");
   }
